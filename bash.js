8b813edafcda87517d47c4f1774d04749154742d
Frances Wang <francesyuanwang@gmail.com>
2018-03-05 14:01:01 -0500

    Updated

--- bash.js
+++ bash.js
@@ -6,22 +6,43 @@ process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
   var cmd = data.toString().trim(); // remove the newline
-
+  var cmdList = cmd.split(/\s*\|\s*/g)
+  // console.log(cmdList)
+  var output = null
+  var input = cmdList[0].split(' ')[1]
+  for (let i = 0; i < cmdList.length; i++) {
+    // if (cmdList[i].indexOf(' ') === -1) {
+    //   console.log(cmdList[i])
+    // } else {
+    //   console.log(cmdList[0].split(' ')[0])
+    // }
+    // console.log(cmdList[i])
+  }
+  // console.log(cmdList[0].split(' ')[1])
   if (cmd === 'pwd') {
-    commands.pwd();
+    commands.pwd(done);
   } else if (cmd === 'date') {
     console.log(Date.now());
   } else if (cmd === 'ls') {
-    commands.ls();
+    commands.ls(done);
   } else if (cmd.slice(0, 5) === 'echo ') {
-    commands.echo(cmd.slice(5));
+    commands.echo(cmd.slice(5), done);
   } else if (cmd.slice(0, 4) === 'cat ') {
-    commands.cat(cmd.slice(4));
+    commands.cat(cmd.slice(4), done);
   } else if (cmd.slice(0, 5) === 'head ') {
-    commands.head(cmd.slice(5));
+    // var arg = cmdList[0].split(' ')[1]
+    // commands.head(arg, cmd.slice(5), done);
+    commands.head(stdin, cmd.slice(5), done)
   } else if (cmd.slice(0, 5) === 'tail ') {
-    commands.tail(cmd.slice(5));
+    commands.tail(cmd.slice(5), done);
+  } else if (cmd.slice(0, 5) === 'curl ') {
+    commands.curl(cmd.slice(5), done);
   }
-  process.stdout.write('\nprompt > ');
+
 
 });
+
+var done = function(output) {
+  console.log(output)
+  process.stdout.write('\nprompt > ');
+}
--- commands.js
+++ commands.js
@@ -1,47 +1,66 @@
 var exports = module.exports = {};
 var fs = require('fs');
+var request = require('request');
 
-exports.pwd = function() {
-  console.log(process.cwd());
+exports.pwd = function(done) {
+  var output = '';
+  output += process.cwd()
+  done(output)
 };
 
-exports.ls = function() {
+exports.ls = function(done) {
+  var output = '';
   fs.readdir('.', function(err, files) {
     if (err) throw err;
     files.forEach(function(file) {
-      process.stdout.write("\n" + file.toString() + " ");
+      output += "\n" + file.toString() + " ";
     })
-    process.stdout.write("\nprompt > ");
+    done(output)
   });
 }
 
-exports.echo = function(data) {
-  process.stdout.write(data);
+exports.echo = function(data, done) {
+  var output = '';
+  output += data
+  done(output)
 }
 
-exports.cat = function(filename) {
+exports.cat = function(filename, done) {
   fs.readFile(filename, 'utf8', (err, data) => {
     if (err) throw err;
-    console.log(data);
+    done(data)
   });
 }
 
-exports.head = function(filename) {
-  fs.readFile(filename, 'utf8', (err, data) => {
+exports.head = function(stdin, filename, done) {
+  var output = '';
+  fs.readFile(stdin || filename, 'utf8', (err, data) => {
     if (err) throw err;
     let lines = data.split('\n');
     for (let i = 0; i < 5; i++) {
-      console.log(lines[i]);
+      output += '\n' + lines[i]
     }
+    done(output)
   });
 }
 
-exports.tail = function (filename) {
-  fs.readFile(filename, 'utf8', (err, data) => {
+exports.tail = function(stdin, filename, done) {
+  var output = '';
+  fs.readFile(stdin || filename, 'utf8', (err, data) => {
     if (err) throw err;
     let lines = data.split('\n');
-    for (let i = lines.length-6; i < lines.length; i++) {
-      console.log(lines[i]);
+    for (let i = lines.length - 6; i < lines.length; i++) {
+      output += '\n' + lines[i]
     }
+    done(output)
   });
+}
+
+exports.curl = function(url, done) {
+  request(url, function(err, response, body) {
+    // output += 'error:' + err
+    // output += 'statusCode:' + response + response.statusCode
+    // output += 'body:' + body
+    done(body)
+  })
 }
